79fe84b566a59085d45703ea6ae521259816cd39
dancanyego <dancankipchumba76@gmail.com>
2021-11-25 19:30:37 +0300

    Ui components

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.notesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import com.example.notesapp.ui.theme.CleanArchitectureNoteAppTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setContent(){
+            CleanArchitectureNoteAppTheme() {
+                
+            }
 
         }
     }
