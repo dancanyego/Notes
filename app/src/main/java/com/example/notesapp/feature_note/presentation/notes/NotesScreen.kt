cc86bdb5f8484e3ab19794f5da0c2e85ef2ba5b9
dancanyego <dancanyego76@gmail.com>
2021-11-29 23:55:36 +0300

    NotesScreen

---
+++ app/src/main/java/com/example/notesapp/feature_note/presentation/notes/NotesScreen.kt
@@ -0,0 +1,29 @@
+package com.example.notesapp.feature_note.presentation.notes
+
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+@@Composable
+fun NotesScreen(
+    navController: NavController,
+    viewModel: NotesViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { }) {
+
+            }
+        }
+    ) {
+    }
+
+}
