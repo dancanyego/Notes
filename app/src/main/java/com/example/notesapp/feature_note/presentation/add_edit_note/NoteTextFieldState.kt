8042010dede42830ca2da8c3a381da25c9378ba9
dancanyego <dancankipchumba76@gmail.com>
2021-12-18 08:03:38 +0300

    note text field state

---
+++ app/src/main/java/com/example/notesapp/feature_note/presentation/add_edit_note/NoteTextFieldState.kt
@@ -0,0 +1,8 @@
+package com.example.notesapp.feature_note.presentation.add_edit_note
+
+
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
